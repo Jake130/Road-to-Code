3fe1f6dbd178626312d536a9f1f5a844c1de04c4
Jake130 <115515270+Jake130@users.noreply.github.com>
2025-02-28 10:38:45 -0800

    Added Separate page for Concepts & About Page; organized accordingly; created basic NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,2 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React from 'react';
+//import React, {useState, useEffect} from 'react';
+import '../css/NavBar.css';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to={"/"}>Welcome</Link></li>
+                <li><Link to={"/"}>Concepts</Link></li>
+                <li><Link to={"/about"}>About</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,39 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 //Import Components
-//import App from './App';
-import Title from './components/Title.js';
 import Body from './components/Body.js';
-import Bubble from './components/Bubble.js';
-import Concept from './components/concept.js';
 
-//Import Images
-import recursion from './imgs/recursion.png';
 
-const concepts = [
-  { name:'Data Types'},
-  { name:'Variables'},
-  { name:'Loops'},
-  { name:'Functions'},
-  { name:'Recursion', src:recursion},
-];
-const conceptList = concepts.map(concept => <Concept text={concept.name} src={concept.src}/>);
-//console.log(conceptList);
+//Router
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+//Pages
+import Concepts from './pages/concepts.js';
+import About from './pages/about.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // put <App /> back in to see default React stuff
 root.render(
   <React.StrictMode>
-      <Body>
-        <div className='title-bubble'>
-          <Title>Concepts Available!</Title>
-        </div>
-        {conceptList}
-        <Concept text="Variables" src={recursion}/>
-        <Bubble className='text-bubble'>Text</Bubble>
-        <Bubble className='title-bubble'>Here</Bubble>
-      </Body>
-      
+    <Body>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Concepts/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Body>
   </React.StrictMode>
 );
 
